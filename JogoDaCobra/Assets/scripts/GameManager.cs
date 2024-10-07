f13a84b151f71458fa6fde08e618126724ec7aed
semimarufbi <gabicolzani@gmail.com>
2024-10-07 14:23:16 -0300

    terminadometodostart

---
+++ JogoDaCobra/Assets/scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region
+    GameManager instnce;
+    private void Awake()
+    {
+        instnce = this;
+    }
+    #endregion
+    public int diametroDoCampo;
+    public int[,] grade;
+    UImanager managerUI;
+    GameObject menu, gameover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        managerUI = GetComponent<UImanager>();
+        menu = GameObject.Find("menu");
+        gameover = GameObject.Find("GameOverPanel");
+        GerarGrade();
+    }
+
+    
+}
