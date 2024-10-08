a0ef9ca1147840cc12a75056a2482ad8966af796
semimarufbi <gabicolzani@gmail.com>
2024-10-08 11:11:13 -0300

    fim metodotoupdate

---
+++ JogoDaCobra/Assets/scripts/camera infinita.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerainfinita : MonoBehaviour
+{
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //inicializa a camera principal
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
